289f61757ea6be67da840cfa257cba53d97cd119
snadell <sravya.nadella7@gmail.com>
2017-07-02 17:59:42 -0400

    added

---
+++ def/rematch.py
@@ -0,0 +1,9 @@
+
+# Ask player for a rematch
+    rematch = raw_input('Would you like to play again? y/n')
+    if rematch == 'y':
+        play_game()
+    else:
+        print "Thanks for playing!"
+    
+
